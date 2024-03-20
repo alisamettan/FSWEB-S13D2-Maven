bd845fe17b871d2b13599f7dcc9ae7d336520fd1
alisamettan <alisamet_tan@hotmail.com>
2024-03-20 10:39:30 +0100

    Mükemmel sayı taski yapıldı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -30,8 +30,17 @@ public class Main {
         return originalNumber==reversedNumber;
     }
 
-    public static double isPerfectNumber(int i){
+    public static boolean isPerfectNumber(int num){
+        if(num<0){
+            return false;
+        }
 
+        int total=0;
+
+        for(int i=1;i<=num/2;i++){
+            if(num%i==0) total+=i;
+        }
+        return  num==total;
     }
 
 
